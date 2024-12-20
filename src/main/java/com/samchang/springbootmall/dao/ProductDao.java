ab342f979047d0d50bfa535b28968d2ba5d15476
changtingmao <changtingmao@yahoo.com.tw>
2024-12-21 01:29:39 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/samchang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/samchang/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.samchang.springbootmall.dao;
 import com.samchang.springbootmall.dto.ProductRequest;
 import com.samchang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/samchang/springbootmall/service/ProductService.java
+++ src/main/java/com/samchang/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.samchang.springbootmall.service;
 import com.samchang.springbootmall.dto.ProductRequest;
 import com.samchang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
